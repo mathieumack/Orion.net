415e888bfa4198b95eaa6bcb97922ff8686836c9
MARION CADIER <marion.cadier@econocom.com>
2020-01-24 14:37:19 +0100

    Problem assembly compilation

--- Orion.Net.Client.UnitTests/ConnectorClient/TestAddCommands.cs
+++ Orion.Net.Client.UnitTests/ConnectorClient/TestAddCommands.cs
@@ -3,6 +3,7 @@ using Moq;
 using Microsoft.AspNetCore.SignalR;
 using Orion.Net.Client.Configuration;
 using Orion.Net.Scripts.Common.Diagnostics;
+using System;
 
 namespace Orion.Net.Client.UnitTests.ConnectorClient
 {
@@ -16,9 +17,9 @@ namespace Orion.Net.Client.UnitTests.ConnectorClient
         /// "System.IO.FileNotFoundException: Could not load file or assembly 
         /// 'Microsoft.Bcl.AsyncInterfaces, Version=1.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'. 
         /// Le fichier spécifié est introuvable."
-        /// yet the file exists where it should be... (path in <see cref="IAsyncDisposable"</remarks>
+        /// yet the file exists where it should be... (path in <see cref="IAsyncDisposable"/>)</remarks>
         [TestMethod]
-        public void ConnectorTestAddCommand()
+        public void TestAddCommandValid()
         {
             //Mock<Connector> mockConnector = new Mock<Connector>();
             //Connector test = mockConnector.Object;
@@ -32,6 +33,23 @@ namespace Orion.Net.Client.UnitTests.ConnectorClient
             test.AddCommandService(new ExecuteProcessClientScript(test));
 
             Assert.AreEqual(1, test.commands.Count);
+        }
+
+        [TestMethod]
+        public void TestAddCommandNull()
+        {
+            Connector test = new Connector();
+
+            ExecuteProcessClientScript testScript = null;
+
+            try
+            {
+                test.AddCommandService(testScript);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.ToString());
+            }
         }
     }
 }
