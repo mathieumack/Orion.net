8dd2580651d06f57a3aaf78f485b3edc8c8cd2ef
MARION CADIER <marion.cadier@econocom.com>
2020-01-24 10:16:21 +0100

    Error : assembly Microsoft.Bcl.AsyncInterfaces

--- Orion.Net.Client.UnitTests/ConnectorClient/TestAddCommands.cs
+++ Orion.Net.Client.UnitTests/ConnectorClient/TestAddCommands.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Microsoft.AspNetCore.SignalR;
 using Orion.Net.Client.Configuration;
@@ -13,7 +12,11 @@ namespace Orion.Net.Client.UnitTests.ConnectorClient
         /// <summary>
         /// Add a command service, as <see cref="ExecuteProcessClientScript"/>, to commands in <see cref="Connector"/>
         /// </summary>
-        /// <remarks> Not working, why ?? What does WPF have that the test doesn't</remarks>
+        /// <remarks> Not working, error :
+        /// "System.IO.FileNotFoundException: Could not load file or assembly 
+        /// 'Microsoft.Bcl.AsyncInterfaces, Version=1.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51'. 
+        /// Le fichier spécifié est introuvable."
+        /// yet the file exists where it should be... (path in <see cref="IAsyncDisposable"</remarks>
         [TestMethod]
         public void ConnectorTestAddCommand()
         {
