b9f79a37ba6464d1a6eddcb470c94dd6d0ca5538
MARION CADIER <marion.cadier@econocom.com>
2020-01-27 12:23:23 +0100

    less using

--- Orion.Net.Client/Configuration/Connector.cs
+++ Orion.Net.Client/Configuration/Connector.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Client;
-using Newtonsoft.Json;
 using Orion.Net.Client.Scripts;
 using Orion.Net.Core.Interfaces;
 using Orion.Net.Core.Scripts;
