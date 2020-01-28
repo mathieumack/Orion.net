fcafcfd205530c9dd3b5e7d669cb8ec9f7a6a04a
MARION CADIER <marion.cadier@econocom.com>
2020-01-28 16:02:42 +0100

    Redis example working, now hide azure key and implement it properly

--- Orion.Net/Program.cs
+++ Orion.Net/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using StackExchange.Redis;
+using System;
+using System.Configuration;
 
 namespace Orion.Net
 {
@@ -13,5 +16,19 @@ namespace Orion.Net
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            string cacheConnection = ConfigurationManager.AppSettings["CacheConnection"].ToString();
+            return ConnectionMultiplexer.Connect(cacheConnection);
+        });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
     }
 }
--- Orion.Net/Startup.cs
+++ Orion.Net/Startup.cs
@@ -10,6 +10,7 @@ namespace Orion.Net
 {
     public class Startup
     {
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
